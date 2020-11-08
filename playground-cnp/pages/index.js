5a400e9370b3e1abf12ac230687d6d935aefc0cd
Arman <arman.khashei@gmail.com>
2020-11-08 13:53:34 -0800

    Init

--- playground-cnp/pages/index.js
+++ playground-cnp/pages/index.js
@@ -1,23 +1,13 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import Example from '../components/Example';
+import Button from '../../dist/Button/index';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <div>
-          <Example />
-        </div>
+        <div>hello</div>
+        <Button />
       </main>
     </div>
   );
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+var path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  // mode: 'production',
+  entry: { index: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+    alias: {
+      myutils: './utils',
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: '.',
+          globOptions: {
+            ignore: [
+              '**/*.stories.js',
+              '**/stories/**',
+              '**/setupTests.js',
+              '**/node_modules/**',
+              '**/playground-cnp/**',
+              '**/package-lock.json',
+              '**/.git',
+              '**/.npmrc',
+              '**/dist',
+              '**/.storybook',
+            ],
+          },
+        },
+      ],
+      options: {
+        concurrency: 100,
+      },
+    }),
+  ],
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    },
+  },
+  module: {
+    rules: [
+      // I dont need this, cant I not use module at all? just want the copy and paste part for now
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+    ],
+    // rules: [ // NO NEED
+    //   {
+    //     test: /\.(js|jsx)$/,
+    //     exclude: /node_modules/,
+    //     use: {
+    //       loader: 'babel-loader',
+    //     },
+    //   },
+    // ],
+  },
+  optimization: {
+    minimize: false,
+  },
+};
